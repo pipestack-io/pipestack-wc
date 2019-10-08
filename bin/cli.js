d24fbc956387749c78387c45991bb0760bea8828
Tomás <tomasbozzo@gmail.com>
2019-10-09 00:04:47 +0200

    WIP

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const wc = require('../lib/wc');
 
-wc.run(process.argv.slice(2), process.stdin);
+process.exit(wc(process.argv.slice(2), process.stdin, process.stdout, process.stderr));
